0c30781650518dc2e56d65df3dfe2c3f1910f1e4
Ayansh Varma <ayanshvarma0603@gmail.com>
2026-08-07 14:10:02 -0700

    chore: convert png to ico

--- app.py
+++ app.py
@@ -30,10 +30,10 @@ icon_src = f"data:image/png;base64,{icon_base64}"
 
 
 st.set_page_config(
-    page_title="Amicus — Understand any legal document",
+    page_title="Amicus - Understand any legal document",
+    page_icon="./AmicusIcon.ico",
     layout="centered",
     initial_sidebar_state="collapsed",
-    page_icon="⚖️",
 )
 
 if "show_camera" not in st.session_state:
